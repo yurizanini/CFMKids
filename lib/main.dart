99a3128e57201ddf7fe6e451ae34c45ab2e71196
Ronald Britos <rmbgkpo@ensign.edu>
2023-01-23 21:53:35 -0700

    Main page draft
    just the main page after clicking on the login button, after login button should be the login option but we can change that later

--- lib/main.dart
+++ lib/main.dart
@@ -40,6 +40,7 @@ class HomePage extends StatelessWidget {
                             minWidth: double.infinity,
                             height: 60,
                             onPressed: () {
+                              Navigator.push(context, Pagetwo());
                               ScaffoldMessenger.of(context).showSnackBar(
                                 const SnackBar(
                                   content: Text("Login Button Pressed"),
@@ -93,4 +94,42 @@ class HomePage extends StatelessWidget {
                   ],
                 ))));
   }
+}
+class Pagetwo extends MaterialPageRoute<Null> {
+  Pagetwo()
+      : super(builder: (BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main Page'),
+  actions: <Widget>[
+          PopupMenuButton<String>(
+            onSelected: handleClick,
+            itemBuilder: (BuildContext context) {
+              return {'Logout', 'Settings'}.map((String choice) {
+                return PopupMenuItem<String>(
+                  value: choice,
+                  child: Text(choice),
+                );
+              }).toList();
+            },
+          ),
+        ],
+      ),
+        
+      
+      body: const Center(
+        child: Text(
+          'This will be the main page',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  });
+}void handleClick(String value) {
+    switch (value) {
+      case 'Logout':
+        break;
+      case 'Settings':
+        break;
+    }
 }
