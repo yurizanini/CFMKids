eb8afd2734f13ffeae2a81bbb5dbc6cc8b730f45
aumfreire <amf212@ensign.edu>
2023-01-30 15:10:54 -0700

    Fixing errors

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,8 @@ import 'package:cfmkids/login.dart';
 import 'package:cfmkids/signup.dart';
 import 'package:flutter/material.dart';
 
-import 'mainpage.dart';
-
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     home: HomePage(),
   ));
@@ -55,18 +53,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final int _counter = 0;
 
   @override
   Widget build(BuildContext context) {
--- lib/signup.dart
+++ lib/signup.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class SignupPage extends StatelessWidget {
+  const SignupPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
