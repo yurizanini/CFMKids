9031e547602b3aa32b2c65c18847f086e2823ccf
aumfreire <amf212@ensign.edu>
2023-01-30 14:35:48 -0700

    Bottom Navigation Icons

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -1,39 +1,78 @@
 import 'package:flutter/material.dart';
-class  MainPage extends MaterialPageRoute<void> {
+
+class MainPage extends MaterialPageRoute<void> {
   MainPage()
       : super(builder: (BuildContext context) {
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Main Page'),
+              actions: <Widget>[
+                PopupMenuButton<String>(
+                  onSelected: handleClick,
+                  itemBuilder: (BuildContext context) {
+                    return {'Logout', 'Settings'}.map((String choice) {
+                      return PopupMenuItem<String>(
+                        value: choice,
+                        child: Text(choice),
+                      );
+                    }).toList();
+                  },
+                ),
+              ],
+            ),
+            body: const HomeScreen(),
+          );
+        });
+}
+
+void handleClick(String value) {
+  switch (value) {
+    case 'Logout':
+      break;
+    case 'Settings':
+      break;
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  void onTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Main Page'),
-  actions: <Widget>[
-          PopupMenuButton<String>(
-            onSelected: handleClick,
-            itemBuilder: (BuildContext context) {
-              return {'Logout', 'Settings'}.map((String choice) {
-                return PopupMenuItem<String>(
-                  value: choice,
-                  child: Text(choice),
-                );
-              }).toList();
-            },
-          ),
-        ],
-      ),
-        
-      
-      body: const Center(
-        child: Text(
-          'This will be the main page',
-          style: TextStyle(fontSize: 24),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+            BottomNavigationBarItem(icon: Icon(Icons.share), label: 'Share'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.notifications), label: 'Notifications'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.lightBlue,
+          unselectedItemColor: Colors.grey,
+          onTap: onTapped,
         ),
-      ),
-    );
-  });
-}void handleClick(String value) {
-    switch (value) {
-      case 'Logout':
-        break;
-      case 'Settings':
-        break;
-    }
+        body: const Center(
+          child: Text(
+            'This will be the main page',
+            style: TextStyle(fontSize: 24),
+          ),
+        ));
+  }
 }
